ecfd9ec5e1c8f19bc995019df5d843d71570c0d1
Samuel Tochetto <samueltochetto@gmail.com>
2022-01-19 16:34:57 -0300

    Meu primeiro Bot de Twitter - Limitando operacoes para evitar block

--- server.js
+++ server.js
@@ -13,23 +13,18 @@ const Tweet = new Twitter({
 function action(tweet){
   const {retweeted_status, id_str, screen_name, is_quote_status} = tweet;
 
-	//console.log ('Validando condicoes Tweet:' +tweet.user.id);
-	//console.log('Texto do Tweet:' + tweet.text);
+	// Quem enviou o tweet?
+	console.log ('Validando condicoes Tweet:' +tweet.user.id);
+	//Qual éo texto?
+	console.log('Texto do Tweet:' + tweet.text);
 
   if(!retweeted_status && !is_quote_status && tweet.user.id != '1483146482844934100' ){ // Se o status não for um retweet normal, nem um retweet com comentário
     
-  // Who sent the tweet?
     var name = tweet.user.screen_name;
-    // What is the text?
-    // var txt = tweet.text;
-    // the status update or tweet ID in which we will reply
     var nameID  = tweet.id_str;
 
-     // Get rid of the @ mention
-    // var txt = txt.replace(/@myTwitterHandle/g, "");
 
-
-	console.log("Iniciando uma curtida e uma resposta :" + name);
+	console.log("Iniciando uma curtida :" + name);
 	
 	
 	Tweet.post('favorites/create', {id: id_str}, erro => { // Dar like no tweet
@@ -44,21 +39,7 @@ function action(tweet){
     })
 
 
-// Iniciar um reply
-    var reply = "Opa! Escutei as palavras mágicas *Livro e *BBB! @" + name + ' ' + ' Vai ler um livro ao invés de BBB esse ano ?';
-    var params             = {
-                              status: reply,
-                              in_reply_to_status_id: nameID
-                             };
-
-    Tweet.post('statuses/update', params, function(err, data, response) {
-      if (err !== undefined) {
-        console.log(err);
-      } else {
-        console.log('Tweeted: ' + params.status);
-      }
-    })
-    
+   
 	 
 	 
   }else {
